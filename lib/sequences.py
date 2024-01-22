b371b8a9ef92415301f88349cb31d294fd2f2698
Jacob Sendzik <jake.sendzik@gmail.com>
2024-01-22 12:01:25 -0500

    Complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    if length == 0:
+        list.clear()
+        print(list)
+    else:
+        print(list[0:length])
